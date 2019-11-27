7cbff2615014ba597157ee898fb3db9d0d61431d
Roman Lytvynenko <roman.lytvynenko@yahoo.com>
2019-11-27 14:39:57 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Roman Lytvynenko on 27.11.2019.
+//  Copyright © 2019 Roman Lytvynenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
